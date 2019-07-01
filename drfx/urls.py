988f96270d0fd738ef8ca482d292122a4971a582
Nikolay Bely <belyynikolay@gmail.com>
2019-07-01 15:00:16 +0700

    Remove all

--- drfx/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import include, path, re_path
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/v1/', include('api.urls')),
-    path('', include('yasg.urls'))
-]
