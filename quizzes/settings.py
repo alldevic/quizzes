7c7b6eaf53dd129ff5b883de3e14a6bf0c63f520
Nikolay Bely <belyynikolay@gmail.com>
2019-07-03 08:40:19 +0700

    Add auth views

--- quizzes/settings.py
+++ quizzes/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'quizzes.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['./templates', ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -124,3 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL = '/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
--- quizzes/urls.py
+++ quizzes/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('checklist/', include('checklist.urls')),
     path('', RedirectView.as_view(url='/checklist/', permanent=True)),
+    path('accounts/', include('django.contrib.auth.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
