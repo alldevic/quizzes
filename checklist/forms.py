21e1b346abe3759a78f74144702e3eee0c477d39
Nikolay Bely <belyynikolay@gmail.com>
2019-07-04 13:45:00 +0700

    remove all

--- checklist/forms.py
+++
@@ -1,9 +0,0 @@
-from django.forms.models import BaseInlineFormSet
-from django.forms.models import inlineformset_factory
-from django import forms
-from django.forms import inlineformset_factory
-
-from .models import Checklist, Question, Answer, AnsweredQuestion, ChecklistInstance
-
-ChecklistFormset = inlineformset_factory(
-    ChecklistInstance, AnsweredQuestion, fields="__all__")
--- checklist/urls.py
+++
@@ -1,9 +0,0 @@
-from . import views
-from django.urls import path
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('create/', views.ChecklistEntityCreate.as_view()),
-    path('qqq/', views.create_book_normal, name='qqq'),
-
-]
